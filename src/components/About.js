2e8ea1d37b78ae2e5fb168c16ce107aedb6c21db
Brandon Moreno <brandonm99@me.com>
2022-10-12 20:30:18 -0600

    commit tree bfc99eb1fb5359cad40736a2593b0ad3d829bf7b

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,14 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id = "about">
+      <h2>About Me</h2>
+      <p> coding is FUNNN</p>
+      <img src= {image} alt="I made this"/>
+
+    </div>
+  );
 }
 
 export default About;
